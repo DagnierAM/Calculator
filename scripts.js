35bfd5419a60d41740f19b2b2c8a0927ac73ed8c
Dagnier <dagniermorales@icloud.com>
2022-04-26 12:06:26 -0400

    Assign variables

---
+++ scripts.js
@@ -0,0 +1,16 @@
+
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const previousOpTxtElement = document.querySelector('[data-previous-operand]');
+const currentOpTxtElement = document.querySelector('[data-current-operand]');
+
+
+
+
+
+
+
+
